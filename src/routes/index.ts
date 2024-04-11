bca295d1e0f9ff42d6e8cfea768906f242b2b989
KukuhAgung <ff.agri2567@gmail.com>
2024-04-11 23:19:17 +0700

    test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Application, Router } from 'express'
 import { ReviewRouter } from './review'
 
-const _routes: Array<[string, Router]> = [['/review', ReviewRouter]]
+const _routes: Array<[string, Router]> = [["'/review'", ReviewRouter]]
 
 export const routes = (app: Application) => {
   _routes.forEach((route) => {
