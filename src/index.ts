2d2577c979d317f46afdfe7e44df3cf0ea5aa441
KukuhAgung <ff.agri2567@gmail.com>
2024-04-11 22:31:55 +0700

    Setup Eslint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { type Application, type Request, type Response, type NextFunction } from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 
 const app: Application = express()
 const port: number = 4000
